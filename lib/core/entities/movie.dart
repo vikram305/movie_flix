c6c2907f7eadad5018e7795f927a046862bdd4a6
Vikram Maurya <vikrammaurya@Vikrams-MacBook-Air.local>
2024-06-17 15:47:45 +0530

    basic folders created and movie entity created

---
+++ lib/core/entities/movie.dart
@@ -0,0 +1,14 @@
+class Movie {
+  final int id;
+  final String title;
+  final bool adult;
+  final String backdrop;
+  final String poster;
+
+  Movie(
+      {required this.id,
+      required this.title,
+      required this.adult,
+      required this.backdrop,
+      required this.poster});
+}
